c1be97beeb25c7557c9acd37f44eeed61d45aabb
Sophie <tengxxu@gmail.com>
2021-07-04 23:16:22 -0400

    Update index page. Add new Fact model

---
+++ AllAboutCat/AllAboutCat/Model/Fact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllAboutCat.Model
+{
+    public class Fact
+    {
+        [Key]
+        [HiddenInput]
+        [Required]
+        public int FactID { get; set; }
+
+        public string FactText { get; set; }
+    }
+}
