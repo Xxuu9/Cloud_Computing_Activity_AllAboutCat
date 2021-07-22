2524fc86bd65254cdb77ccb22160b15082bfe8b5
Sophie <tengxxu@gmail.com>
2021-07-22 15:04:56 -0400

    Add new scaffold item

---
+++ CloudComputingAllAboutCat/CloudComputingAllAboutCat/Pages/FactPages/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CloudComputingAllAboutCat.Context;
+using CloudComputingAllAboutCat.Model;
+
+namespace CloudComputingAllAboutCat.Pages.FactPages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CloudComputingAllAboutCat.Context.DataContext _context;
+
+        public DeleteModel(CloudComputingAllAboutCat.Context.DataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Fact Fact { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Fact = await _context.Facts.FirstOrDefaultAsync(m => m.FactID == id);
+
+            if (Fact == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Fact = await _context.Facts.FindAsync(id);
+
+            if (Fact != null)
+            {
+                _context.Facts.Remove(Fact);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CloudComputingAllAboutCat/CloudComputingAllAboutCat/Pages/FactPages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CloudComputingAllAboutCat.Context;
+using CloudComputingAllAboutCat.Model;
+
+namespace CloudComputingAllAboutCat.Pages.FactPages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CloudComputingAllAboutCat.Context.DataContext _context;
+
+        public DetailsModel(CloudComputingAllAboutCat.Context.DataContext context)
+        {
+            _context = context;
+        }
+
+        public Fact Fact { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Fact = await _context.Facts.FirstOrDefaultAsync(m => m.FactID == id);
+
+            if (Fact == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
