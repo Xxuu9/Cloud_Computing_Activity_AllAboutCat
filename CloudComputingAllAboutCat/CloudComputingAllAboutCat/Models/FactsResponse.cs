6e3b95b8b1b14222535e28b2c7a589cbb8e87b94
Sophie <tengxxu@gmail.com>
2021-08-08 22:32:45 -0400

    Add a class to deserialize the json

---
+++ CloudComputingAllAboutCat/CloudComputingAllAboutCat/Models/FactsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudComputingAllAboutCat.Models
+{
+    public class FactsResponse
+    {
+
+        public string fact;
+    }
+
+
+}
+
+
--- CloudComputingAllAboutCat/CloudComputingAllAboutCat/Pages/Facts/Index.cshtml.cs
+++ CloudComputingAllAboutCat/CloudComputingAllAboutCat/Pages/Facts/Index.cshtml.cs
@@ -7,23 +7,47 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CloudComputingAllAboutCat.Data;
 using CloudComputingAllAboutCat.Models;
+using Newtonsoft.Json;
 
 namespace CloudComputingAllAboutCat.Pages.Facts
 {
     public class IndexModel : PageModel
     {
-        private readonly CloudComputingAllAboutCat.Data.FactDbContext _context;
+        private readonly FactDbContext _context;
 
-        public IndexModel(CloudComputingAllAboutCat.Data.FactDbContext context)
+        private string dogFactUrl = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all";
+
+        public IndexModel(FactDbContext context)
         {
             _context = context;
         }
 
-        public IList<Fact> Fact { get;set; }
+        public IList<Fact> Facts { get;set; }
 
         public async Task OnGetAsync()
         {
-            Fact = await _context.Facts.ToListAsync();
+            Facts = await _context.Facts.ToListAsync();
+
+            if (Facts.Count == 0)
+            {
+                string json = new System.Net.WebClient().DownloadString(dogFactUrl);
+                var factsResponse = JsonConvert.DeserializeObject<List<FactsResponse>>(json);
+                foreach (var item in factsResponse)
+                {
+
+                    var fact = item.fact;
+                    Fact a = new Fact()
+                    {
+                        FactText = fact
+                    };
+                    _context.Facts.Add(a);
+ 
+                };
+
+                await _context.SaveChangesAsync();
+
+                Facts = await _context.Facts.ToListAsync();
+            }
         }
     }
 }
